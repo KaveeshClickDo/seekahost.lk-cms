4c55747750f0f95be6556b0a77e9e986fbe545f8
KHSenevirathne <khsenevirathne@gmail.com>
2025-05-28 20:43:22 +0530

    feat: customized main UI texts

---
+++ src/admin/app.tsx
@@ -0,0 +1,14 @@
+export default {
+  config: {
+    translations: {
+      en: {
+        'Auth.form.welcome.title': 'Welcome to SeekaHost',
+        'Auth.form.welcome.subtitle': 'Please log in to continue',
+        'HomePage.header.subtitle' : 'Welcome to SeekaHost administration panel',
+        'Auth.form.email.placeholder' : 'e.g. kai@seekahost.co.uk',
+        'HomePage.head.title' : 'Admin',
+      },
+    },
+  },
+  bootstrap() {},
+};
